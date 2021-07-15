b7f609e3adb112683d9d08f22b71b07f94cdd169
igorhit <igor_hit@yahoo.com.br>
2021-07-15 15:44:02 -0300

    Update 4.sql

--- banco de dados/4.sql
+++ banco de dados/4.sql
@@ -1,22 +1,11 @@
 -- Escrever uma consulta para trazer a quantidade de ligações por CIDADE.
 -- a. Palavras-chave: GROUP BY, COUNT, INNER JOIN
 
--- criando tabela auxiliar
-create table lig_cid as 
 select 
-o.codigocidade as id_cid, 
-count(codigooperador) as ligações 
+c.nome as Cidades,
+count(codigooperador) as Ligações 
 from ligacoes
-inner join operadores o on codigooperador = codigo
+inner join operadores o on codigooperador = o.codigo
+inner join cidades c on o.codigocidade = c.codigo
 group by codigocidade
-
--- consulta de ligações por cidade
-select
-nome as CIDADES,
-ligações as LIGAÇÕES
-from cidades
-inner join lig_cid on id_cid = codigo
-order by ligações desc
-
--- deletando tabela auxiliar
-drop table if exists lig_cid
+order by Ligações desc
